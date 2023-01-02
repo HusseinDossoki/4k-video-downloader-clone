28738185514c195e637ddbbbc13e652d2b16c1eb
Hussein Dossoki <husseindossoki@gmail.com>
2023-01-02 13:08:53 +0200

    Add npm and cargo packages

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
+import "./assets/bootstrap.min.css"
 import "./style.css";
 import App from "./App.vue";
+import { createPinia } from "pinia";
 
-createApp(App).mount("#app");
+createApp(App)
+.use(createPinia() )
+.mount("#app");
