f74c7f11558ce1c35ae32f9044e4907316f0fcb9
Hussein Dossoki <husseindossoki@gmail.com>
2023-01-04 16:39:48 +0200

    Add videos list design

---
+++ src/components/VideosList.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <VideoListItem v-for="(video, index) in videos" :index="index" :key="index" />
+  </section>
+</template>
+
+<script setup>
+import VideoListItem from './VideoListItem.vue';
+import { ref } from "vue";
+
+const videos = ref([1, 2, 1, 1, 1, 1]);
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <AppHeader/>
-  <EmptyList/>
+  <EmptyList v-if="empty" />
+  <VideosList v-if="!empty" />
   <AppFooter/>
 </template>
 
@@ -8,6 +9,11 @@
 import AppHeader from '../components/AppHeader.vue';
 import EmptyList from '../components/EmptyList.vue';
 import AppFooter from '../components/AppFooter.vue';
+import VideosList from '../components/VideosList.vue';
+import {ref} from "vue";
+
+const empty = ref(false);
+
 </script>
 
 <style scoped>
